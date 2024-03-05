e8b4c621dedf439b372586926b672c3d3db9b1b8
Alex Rosa <alexr@gec.inatel.br>
2024-03-04 21:28:13 -0300

    Experiment with others animations

--- app/src/main/java/com/example/woof/MainActivity.kt
+++ app/src/main/java/com/example/woof/MainActivity.kt
@@ -21,10 +21,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -143,6 +145,10 @@ fun DogItem(
     Use the mutableStateOf() function so Compose observes any changes to the state value, and triggers a recomposition to update the UI. Wrap the mutableStateOf() function call with the remember() function to store the value in the Composition during  initial composition, and the stored value is returned during recomposition.
      */
     var expanded by remember { mutableStateOf(false) }
+    val color by animateColorAsState(
+        targetValue = if (expanded) MaterialTheme.colorScheme.tertiaryContainer
+        else MaterialTheme.colorScheme.primaryContainer
+    )
 
     Card(modifier = modifier) {
         Column(
@@ -153,6 +159,7 @@ fun DogItem(
                         stiffness = Spring.StiffnessMedium
                     )
                 )
+                .background(color)
         ) {
             Row(
                 modifier = Modifier
